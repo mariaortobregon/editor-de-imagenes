bc60eea2b617f9163beca73ede8b4f447ca2c35b
pilar ortobre <pilitagames2012@gmail.com>
2026-05-22 12:36:30 -0600

    Update image_filters.py

--- image_filters.py
+++ image_filters.py
@@ -36,5 +36,24 @@ def display_image(self, img):
     qimg = QImage(data, w, h, w * 3, QImage.Format_RGB888)
     pixmap = QPixmap.fromImage(qimg)
     self.image_Label.setPixmap(pixmap)
-def sepia_image(aelf):
-    pass
+def apply_sepia(image):
+    pixels = image.load()
+    for y in range(image.height):
+        for x in range(image.width):
+            r, g, b = pixels[x, y]
+            transform_red   = int(0.393*r + 0.769*g + 0.189*b)
+            transform_green = int(0.349*r + 0.686*g + 0.168*b)
+            transform_blue  = int(0.272*r + 0.534*g + 0.131*b)
+            pixels[x, y] = (min(255, transform_red),
+                            min(255, transform_green),
+                            min(255, transform_blue))
+    return image
+
+def adjust_brightness(image, factor):
+    enhancer = ImageEnhance.Brightness(image)
+    return enhancer.enhance(factor)
+    
+def adjust_contrast(image, factor):
+    enhancer = ImageEnhance.Contrast(image)
+    return enhancer.enhance(factor)
+    
